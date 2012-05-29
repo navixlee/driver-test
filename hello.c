8bd7d7b96bdae0f8896bb1d2c450ad55a8a8b204
navi <navi@navi-laptop.(none)>
2012-05-29 13:42:45 +0800

    add new file hello.c

---
+++ hello.c
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  05/29/2012 01:40:47 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello Ivan Lee.\n");
+
+	return 0;
+}
+
